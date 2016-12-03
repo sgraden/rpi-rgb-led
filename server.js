d2d0e7fb721d091ea1c6c30457d00e6ede6acf3b
Steven Raden <stevengraden@gmail.com>
2016-12-03 16:13:20 -0600

    reduced to basic color commands

--- server.js
+++ server.js
@@ -17,33 +17,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Each section is doing exactly the same but for a particular color.
 // First, we grab the value and if it is an integer we are dividing it by 255 and sending it to the pi-blaster daemon.
 
-app.get('/red/:value', function (req, res) {
+app.get('/red', function (req, res) {
     console.log("red = " + req.params.value);
     var redValue = req.params.value;
     if( !isNaN( parseInt(redValue) ) ){
-        piblaster.setPwm(RED_GPIO_PIN, redValue/255);
+        piblaster.setPwm(RED_GPIO_PIN, 1);//redValue/255);
         res.send('ok');
     } else {
         res.status(400).send('error');
     }
 });
 
-app.get('/green/:value', function (req, res) {
+app.get('/green', function (req, res) {
     console.log("green = " + req.params.value);
     var greenValue = req.params.value;
     if( !isNaN( parseInt(greenValue) ) ){
-        piblaster.setPwm(GREEN_GPIO_PIN, greenValue/255);
+        piblaster.setPwm(GREEN_GPIO_PIN, 1);//greenValue/255);
         res.send('ok');
     } else {
         res.status(400).send('error');
     }
 });
 
-app.get('/blue/:value', function (req, res) {
+app.get('/blue', function (req, res) {
     console.log("blue = " + req.params.value);
     var blueValue = req.params.value;
     if( !isNaN( parseInt(blueValue) ) ){
-        piblaster.setPwm(BLUE_GPIO_PIN, blueValue/255);
+        piblaster.setPwm(BLUE_GPIO_PIN, 1);//blueValue/255);
         res.send('ok');
     } else {
         res.status(400).send('error');
